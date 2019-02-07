98ea9bd00169da03e394aea1bd4bea85cf96fd92
msm3858 <marcin.msm@gmail.com>
2019-02-07 12:11:56 +0100

    Final.

--- skit/document/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import EmployeeDocument, MeetingDocument
-
-# Register your models here.
-admin.site.register(MeetingDocument)
-admin.site.register(EmployeeDocument)
